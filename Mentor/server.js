8bc22579e95b19f7ceeefa142b722a000e076104
Francis Kuta <129748652+DrRankk@users.noreply.github.com>
2024-04-15 13:42:35 +0300

    Update server.js
    changed port from 5000 to 10000

--- Mentor/server.js
+++ Mentor/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({
     }
   }));
 app.use(bodyParser.urlencoded({ extended: false }))
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cookieParser())
 app.use(session({
